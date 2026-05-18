c0babc2cad2d9a7448aa961a11b2987f0a240bda
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-05-17 22:46:48 -0300

    Ajustes na configuração do servidor com rotas de jogador, headers e options

--- server/src/main/Server.php
+++ server/src/main/Server.php
@@ -3,8 +3,11 @@
 namespace Server\main;
 
 use Server\main\routes\AssombracaoRoutes;
+use Server\main\routes\JogadorRoutes;
 use Server\main\routes\MausoleuRoutes;
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 class Server
 {
@@ -15,10 +18,25 @@ class Server
         $app->addBodyParsingMiddleware();
         $app->addRoutingMiddleware();
 
+        $app->add(function (Request $request, $handler) {
+            $response = $handler->handle($request);
+            
+            return $response
+                ->withHeader('Access-Control-Allow-Origin', '*') 
+                ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization, X-Custom-Header')
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+                ->withHeader('Access-Control-Allow-Credentials', 'true');
+        });
+
         $app->get('/favicon.ico', function ($request, $response) {
             return $response->withStatus(204);
         });
 
+        $app->options('/[{routes:.+}]', function (Request $request, Response $response) {
+            return $response;
+        });
+
+        JogadorRoutes::register($app);
         AssombracaoRoutes::register($app);
         MausoleuRoutes::register($app);
 
