502a3fe667554f0b7c9446ece832eed0b5a49f0d
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-05-17 22:43:41 -0300

    Modelagem da tabela do jogador com dados iniciais

--- server/src/infrastructure/database/mariadb.sql
+++ server/src/infrastructure/database/mariadb.sql
@@ -2,6 +2,18 @@ CREATE DATABASE graveyard_simulator;
 
 USE graveyard_simulator;
 
+CREATE TABLE jogadores(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    energia int NOT NULL,
+    dinheiro int NOT NULL,
+    energia_maxima int NOT NULL
+);
+
+-- inserção do único jogador com valores iniciais
+INSERT INTO jogadores
+(energia, dinheiro, energia_maxima)
+VALUES (200, 0, 200);
+
 CREATE TABLE mausoleus(
     id int PRIMARY KEY AUTO_INCREMENT,
     nome varchar(200) NOT NULL,
