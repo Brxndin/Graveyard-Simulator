1332982f4dce82cba658eb478abfb19fa258ba94
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-05-17 22:55:49 -0300

    Configuração do axios para os repositories

---
+++ app/src/infrastructure/http/config/axios-client.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const axiosClient = axios.create({
+    baseURL: `http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}`,
+    headers: {
+        'Content-Type': 'application/json',
+    }
+});
