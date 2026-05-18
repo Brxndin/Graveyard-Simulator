abd9cee6a64d76e2584b691066779e8c3b0bd3c6
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-05-17 22:57:09 -0300

    Novo tipo de assombração e função para pegar a descrição

--- server/src/domain/enums/TipoAssombracaoEnum.php
+++ server/src/domain/enums/TipoAssombracaoEnum.php
@@ -8,4 +8,16 @@ enum TipoAssombracaoEnum: int
     case Fantasma = 2;
     case Esqueleto = 3;
     case Zumbi = 4;
+    case Demonio = 5;
+
+    public function descricao(): string
+    {
+        return match ($this) {
+            self::Vampiro => 'Vampiro',
+            self::Fantasma => 'Fantasma',
+            self::Esqueleto => 'Esqueleto',
+            self::Zumbi => 'Zumbi',
+            self::Demonio => 'Demônio',
+        };
+    }
 }
