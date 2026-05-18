ec520f555f25ca65471fd6b0f2fc04b82de0df5e
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-05-17 23:09:56 -0300

    Atualização das telas principal e do pátio com o uso dos hooks e dos novos componentes criados

--- app/src/main/pages/PatioPrincipal.tsx
+++ app/src/main/pages/PatioPrincipal.tsx
@@ -1,11 +1,18 @@
+import { useContext } from 'react';
+import imagemPatio from '../assets/patio.jpg';
+import { BotaoAcao } from '../components/BotaoAcao';
 import { BotaoNavegacao } from '../components/BotaoNavegacao';
 import { BoxTexto } from '../components/BoxTexto';
-import { Destinos } from '../enums/Destinos';
-
-// aqui,m além do que já tem, deve ter os botões pra ir pros lugares. Os botões devem chamar o Caminho, passando o destino como prop
-// assim o caminho consegue funcionar e, depois, montar o destino original
+import { Status } from '../components/Status';
+import { Titulo } from '../components/Titulo';
+import { JogoContext } from '../contexts/JogoContext';
+import { Destinos } from '../enums/destinos';
+import { useAtualizarJogador } from '../hooks/useAtualizarJogador';
 
 export const PatioPrincipal = () => {
+    const { jogador, setJogador } = useContext(JogoContext);
+    const { jogadorAtualizado, atualizarJogador, error } = useAtualizarJogador();
+
     // verificar se não é melhor pegar esses dados de um repository mockado mesmo
     const descricoes = [
         'Você chega no pátio principal, um lugar que, em outro contexto, seria colorido e alegre, mas que aqui, tanto no dia quanto na escuridão da noite, tem um ar fúnebre e triste, como se perdesse a cor. Você consegue ver a lua cheia soltando seu fraco brilho azul, mas que já é o suficiente para dissernir os arredores.',
@@ -15,11 +22,52 @@ export const PatioPrincipal = () => {
 
     return (
         <div>
-            <h1>Pátio Principal</h1>
-            <BoxTexto descricoes={descricoes} />
-            <BotaoNavegacao valor="Igreja" destino={`caminho/${Destinos.Igreja}`} />
-            <BotaoNavegacao valor="Necrotério" destino={`caminho/${Destinos.Necroterio}`} />
-            <BotaoNavegacao valor="Mausoléus" destino={`caminho/${Destinos.Mausoleus}`} />
+            <Titulo texto="Pátio Principal" />
+            <Status dinheiro={jogador?.dinheiro} energia={jogador?.energia} energiaMaxima={jogador?.energiaMaxima} />
+            <p>{error}</p>
+            <BoxTexto descricoes={descricoes} imagem={imagemPatio} />
+            <div className="center">
+                <BotaoAcao
+                    valor="Varrer"
+                    acao={() => {
+                        if (jogador) {
+                            atualizarJogador({
+                                jogador: jogador,
+                                dinheiro: 100,
+                                energia: -10,
+                                energiaMaxima: 0,
+                            });
+
+                            // verificar isso pois está bugado
+                            // não sei se é nesse if ou no hook
+                            // na primeira request ele da certo e na segunda falha dizendo que nenhum dado foi alterado
+                            if (!error) {
+                                setJogador(jogadorAtualizado);
+                            }
+                        }
+                    }}
+                />
+                <BotaoAcao
+                    valor="Descansar"
+                    acao={() => {
+                        if (jogador) {
+                            atualizarJogador({
+                                jogador: jogador,
+                                dinheiro: 0,
+                                energia: 100,
+                                energiaMaxima: 0,
+                            });
+
+                            if (!error) {
+                                setJogador(jogadorAtualizado);
+                            }
+                        }
+                    }}
+                />
+                <BotaoNavegacao valor="Igreja" destino={`caminho/${Destinos.Igreja}`} />
+                <BotaoNavegacao valor="Funerária" destino={`caminho/${Destinos.Funeraria}`} />
+                <BotaoNavegacao valor="Mausoléus" destino={`caminho/${Destinos.Mausoleus}`} />
+            </div>
         </div>
     );
 };
--- app/src/main/pages/TelaInicial.tsx
+++ app/src/main/pages/TelaInicial.tsx
@@ -1,12 +1,34 @@
+import { useContext, useEffect } from 'react';
 import { BotaoNavegacao } from '../components/BotaoNavegacao';
-import { Destinos } from '../enums/Destinos';
+import { Paragrafo } from '../components/Paragrafo';
+import { Titulo } from '../components/Titulo';
+import { JogoContext } from '../contexts/JogoContext';
+import { Destinos } from '../enums/destinos';
+import { useBuscarJogador } from '../hooks/useBuscarJogador';
 
 export const TelaInicial = () => {
+    const { setJogador } = useContext(JogoContext);
+    const { jogador, isLoading, buscarJogador } = useBuscarJogador();
+
+    useEffect(() => {
+        buscarJogador(1);
+    }, [buscarJogador]);
+
+    useEffect(() => {
+        if (jogador) {
+            setJogador(jogador);
+        }
+    }, [jogador, setJogador]);
+
+    if (isLoading) return <Paragrafo texto="Acordando os mortos... aguarde." />
+
     return (
         <div>
-            <h1>Graveyard Simulator</h1>
-            <p>Nas noites de luar, com o voar dos morcegos, você deverá caminhar entre os mortos.</p>
-            <BotaoNavegacao valor='Entrar no Cemitério' destino={Destinos.PatioPrincipal}/>
+            <Titulo texto="Graveyard Simulator" />
+            <Paragrafo texto='Nas noites de luar, com o voar dos morcegos, você deverá caminhar entre os mortos' />
+            <div className="center">
+                <BotaoNavegacao valor="Entrar no Cemitério" destino={Destinos.PatioPrincipal} />
+            </div>
         </div>
     );
 };
