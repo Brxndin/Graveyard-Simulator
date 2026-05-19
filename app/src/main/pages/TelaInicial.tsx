177dc9e6af4c5de3860658662fce387aeeec7d42
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-05-18 22:27:16 -0300

    Remoção da busca de jogador somente na tela inicial e ajustes na interface

--- app/src/main/pages/TelaInicial.tsx
+++ app/src/main/pages/TelaInicial.tsx
@@ -1,33 +1,17 @@
-import { useContext, useEffect } from 'react';
 import { BotaoNavegacao } from '../components/BotaoNavegacao';
 import { Paragrafo } from '../components/Paragrafo';
 import { Titulo } from '../components/Titulo';
-import { JogoContext } from '../contexts/JogoContext';
 import { Destinos } from '../enums/destinos';
-import { useBuscarJogador } from '../hooks/useBuscarJogador';
 
 export const TelaInicial = () => {
-    const { setJogador } = useContext(JogoContext);
-    const { jogador, isLoading, buscarJogador } = useBuscarJogador();
-
-    useEffect(() => {
-        buscarJogador(1);
-    }, [buscarJogador]);
-
-    useEffect(() => {
-        if (jogador) {
-            setJogador(jogador);
-        }
-    }, [jogador, setJogador]);
-
-    if (isLoading) return <Paragrafo texto="Acordando os mortos... aguarde." />
-
     return (
         <div>
             <Titulo texto="Graveyard Simulator" />
             <Paragrafo texto='Nas noites de luar, com o voar dos morcegos, você deverá caminhar entre os mortos' />
-            <div className="center">
-                <BotaoNavegacao valor="Entrar no Cemitério" destino={Destinos.PatioPrincipal} />
+            <div className="container-botoes">
+                <div className="box">
+                    <BotaoNavegacao valor="Entrar no Cemitério" destino={Destinos.PatioPrincipal} />
+                </div>
             </div>
         </div>
     );
