26a441ef49de91d7402b77d1675419c9a37b6e45
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-05-24 23:17:31 -0300

    Ajuste na entity do jogador com os novos dados e com novas regras de negócio

--- server/src/domain/entities/Jogador.php
+++ server/src/domain/entities/Jogador.php
@@ -12,12 +12,16 @@ class Jogador
         int $energia,
         int $dinheiro,
         int $energiaMaxima,
+        int $diaAtual,
+        int $assombrometroAtual,
         int | null $id = null,
     ) {
         $this->id = $id;
         $this->energiaMaxima = $energiaMaxima;
         $this->energia = $energia;
         $this->dinheiro = $dinheiro;
+        $this->diaAtual = $diaAtual;
+        $this->assombrometroAtual = $assombrometroAtual;
     }
 
     public int | null $id {
@@ -66,6 +70,28 @@ class Jogador
         }
     }
 
+    public int $diaAtual {
+        get {
+            return $this->props['diaAtual'] ?? 0;
+        }
+
+        set(int $value) {
+            $this->props['diaAtual'] = $value;
+        }
+    }
+
+    public int $assombrometroAtual {
+        get {
+            return $this->props['assombrometroAtual'] ?? 0;
+        }
+
+        set(int $value) {
+            $this->validaAssombrometro($value);
+
+            $this->props['assombrometroAtual'] = $value;
+        }
+    }
+
     private function validaEnergia(int $energia)
     {
         if ($energia < 0) {
@@ -93,5 +119,62 @@ class Jogador
         if ($dinheiro < 0) {
             throw new DomainException('O dinheiro não pode ser menor do que 0!');
         }
+    }
+
+    private function validaAssombrometro(int $validaAssombrometro)
+    {
+        if ($validaAssombrometro > 100) {
+            throw new DomainException('O assombrômetro pode chegar, no máximo, até 100!');
+        }
+    }
+
+    public function varrer(): string {
+        if ($this->energia < 10) {
+            return 'Você tenta, mas está muito cansado para fazer qualquer coisa.';
+        }
+
+        $this->energia -= 10;
+        $this->dinheiro += 100;
+
+        return 'Você varre as folhas secas do pátio, que inevitavelmente ficará sujo novamente com a força do tempo e do vento.';
+    }
+
+    public function acenderVelas(): string {
+        if ($this->energia < 5) {
+            return 'Você tenta, mas está muito cansado para fazer qualquer coisa.';
+        }
+
+        $this->energia -= 5;
+        $this->dinheiro += 50;
+
+        return 'Você acende as velas e fica olhando elas dançarem, criando diversas sombras nos arredores que parecem vivas.';
+    }
+
+    public function exorcizar(): string {
+        if ($this->energia < 80) {
+            return 'Você tenta, mas está muito cansado para fazer qualquer coisa.';
+        }
+
+        $this->energia -= 80;
+        $this->dinheiro += 300;
+
+        return 'Você começa a ajudar o padre no exorcismo. Apesar de muito assustado, você segue o que lhe é dito e, no meio das vozes graves e gritos de agonia, o exorcismo é finalizado com sucesso.';
+    }
+
+    public function descansar(int $medoCausado): string
+    {
+        $this->energia = $this->energiaMaxima;
+        $this->diaAtual += 1;
+        $this->assombrometroAtual += $medoCausado;
+
+        return 'Você descansa e se prepara para mais uma longa noite em meio aos espíritos.';
+    }
+
+    public function isGameOver(): bool {
+        return $this->assombrometroAtual >= 100;
+    }
+
+    public function isVencedor(): bool {
+        return $this->diaAtual >= 20;
     }
 }
