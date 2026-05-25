a544c01cbff2afa974d01c079ed3a35e5d636a1f
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-05-24 23:15:42 -0300

    Mudança nas tabelas do banco de dados, removendo as que não são mais usadas e adicionando dados novos

--- server/src/infrastructure/database/mariadb.sql
+++ server/src/infrastructure/database/mariadb.sql
@@ -6,25 +6,12 @@ CREATE TABLE jogadores(
     id int PRIMARY KEY AUTO_INCREMENT,
     energia int NOT NULL,
     dinheiro int NOT NULL,
-    energia_maxima int NOT NULL
+    energia_maxima int NOT NULL,
+    dia_atual int NOT NULL,
+    assombrometro_atual int NOT NULL
 );
 
 -- inserção do único jogador com valores iniciais
 INSERT INTO jogadores
-(energia, dinheiro, energia_maxima)
-VALUES (200, 0, 200);
-
-CREATE TABLE mausoleus(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    nome varchar(200) NOT NULL,
-    lugares int NOT NULL
-);
-
-CREATE TABLE assombracoes(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    nome varchar(200) NOT NULL,
-    tipo int NOT NULL,
-    mausoleu_id int,
-
-    FOREIGN KEY (mausoleu_id) REFERENCES mausoleus(id)
-);
+(energia, dinheiro, energia_maxima, dia_atual, assombrometro_atual)
+VALUES (200, 0, 200, 1, 0);
