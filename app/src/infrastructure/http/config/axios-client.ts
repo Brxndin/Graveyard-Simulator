ce62a585c4a7eaaefb6d987ee1b47f3d80c141db
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-05-25 20:06:38 -0300

    Remoção do throw do axios para responses maiores que 400

--- app/src/infrastructure/http/config/axios-client.ts
+++ app/src/infrastructure/http/config/axios-client.ts
@@ -4,5 +4,10 @@ export const axiosClient = axios.create({
     baseURL: `http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}`,
     headers: {
         'Content-Type': 'application/json',
+    },
+    validateStatus: function (status) {
+        console.log(status);
+
+        return true;
     }
 });
